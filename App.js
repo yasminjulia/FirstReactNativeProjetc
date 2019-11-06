fdf88f2e2da1a94c45f595706c49d601dec3f5f0
Julia Guimaraes <yasminjulllia@gmail.com>
2019-11-06 00:46:18 -0300

    implementando itens da lista

--- App.js
+++ App.js
@@ -7,25 +7,46 @@
  */
 import lista from './src/lista';
 import React from 'react';
-import styled from 'styled-components/native'
+import styled from 'styled-components/native';
 
 const Page = styled.SafeAreaView `
-flex:1
+flex:1;
 `;
-const Item = styled.View `
+const Scroll = styled.ScrollView `
+flex:1;
+background-color:#FF0000;
+`;
+const Item = styled.TouchableOpacity `
 padding: 10px;
+background-color:#CCC;
 `;
 const ItemText = styled.Text `
 font-size: 13px;
 `;
+const ItemCheck = styled.View '
+width: 20 px;
+height: 20 px;
+border - radius: 10 px;
+border: 5 px solid# FFF;
+';
 
 export default () => {
-    return ( < Page > {
-        lista.map(item => {
-            return ( < Item >
-                <
-                ItemText > { item.task } < /ItemText> </Item >
-            );
-        })
-    } < /Page>)
+    return ( <
+        Page >
+        <
+        Scroll > {
+            lista.map((item, index) => {
+                return ( <
+                    Item key = { index }
+                    activeOpacity = { 0.6 } >
+                    <
+                    ItemText > { item.task } < /ItemText>  <
+                    ItemCheck > < /ItemCheck> <
+                    /Item>
+                );
+            })
+        } <
+        /Scroll> <
+        /Page>
+    )
 }
